04d5bbf46a9d0d9e3fcea28a1a23aa92474125b3
agatav13 <agataomasta05@gmail.com>
2025-03-28 23:03:42 +0100

    Create new column with day of week

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
 
 def process_data(data_path):
     raw_data = pd.read_csv(f"{data_path}/raw_data.csv")
@@ -10,6 +10,10 @@ def process_data(data_path):
 
     # Change the date format from "December 8, 2024 at 07:02PM" to "2024-12-08 19:02"
     raw_data["date"] = raw_data["date"].apply(lambda x: datetime.strptime(x, "%B %d, %Y at %I:%M%p").strftime("%Y-%m-%d %H:%M"))
+    raw_data["date"] = pd.to_datetime(raw_data["date"])
+
+    # Create new column with the day od week (Monday=0, Sunday=6)
+    raw_data["day_of_week"] = raw_data["date"].dt.day_of_week
 
     os.makedirs(data_path, exist_ok=True)
     raw_data.to_csv(f"{data_path}/processed_data.csv", index=False)  # Save to a CSV file
