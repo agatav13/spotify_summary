93ca2a37aaf57abe590b7c8671e873e2579eedf4
agatav13 <agataomasta05@gmail.com>
2025-03-30 20:37:29 +0200

    Add column with time of the day

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,6 +1,21 @@
 import os
 import pandas as pd
-from datetime import datetime, date
+from datetime import datetime
+
+# Morning: 6:00 - 11:59
+# Afternoon: 12:00 - 17:59
+# Evening: 18:00 - 22:59
+# Night: 23:00 - 5:59
+# Returns label for time of the day based on the hour
+def get_time_of_day(hour):
+    if 6 <= hour <= 11:
+        return "Morning"
+    elif 12 <= hour <= 17:
+        return "Afternoon"
+    elif 18 <= hour <= 22:
+        return "Evening"
+    else:
+        return "Night"
 
 def process_data(data_path):
     raw_data = pd.read_csv(f"{data_path}/raw_data.csv")
@@ -14,6 +29,9 @@ def process_data(data_path):
 
     # Create new column with the day od week (Monday=0, Sunday=6)
     raw_data["day_of_week"] = raw_data["date"].dt.day_of_week
+
+    # Create new column with time of the day (Morning, Afternoon, Evening, Night)
+    raw_data["time_of_day"] = raw_data["date"].dt.hour.apply(get_time_of_day)
 
     os.makedirs(data_path, exist_ok=True)
     raw_data.to_csv(f"{data_path}/processed_data.csv", index=False)  # Save to a CSV file
